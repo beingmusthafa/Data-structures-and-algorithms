043bb9a020689a692d6c6498a2bc2b3e005880e9
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-12 01:13:19 +0530

    minor fix

--- data structures/heap/heap.js
+++ data structures/heap/heap.js
@@ -2,77 +2,80 @@ class MaxHeap {
   constructor() {
     this.heap = [];
   }
-
-  // Helper method to swap the nodes
   swap(index1, index2) {
     let temp = this.heap[index1];
     this.heap[index1] = this.heap[index2];
     this.heap[index2] = temp;
   }
 
-  // Method to maintain the heap property during insertion
   heapifyUp() {
     let index = this.heap.length - 1;
     while (index > 0) {
       let element = this.heap[index],
         parentIndex = Math.floor((index - 1) / 2),
         parent = this.heap[parentIndex];
-
-      if (parent >= element) break;
-      this.swap(parentIndex, index);
+      if (element <= parent) break;
+      this.swap(index, parentIndex);
       index = parentIndex;
     }
   }
 
-  // Method to maintain the heap property during deletion
   heapifyDown() {
     let index = 0,
-      length = this.heap.length,
-      element = this.heap[0];
-
+      length = this.heap.length;
     while (true) {
-      let leftChild = 2 * index + 1,
-        rightChild = 2 * index + 2,
+      let left = 2 * index + 1,
+        right = 2 * index + 2,
         swap = null;
-
-      if (leftChild < length) {
-        if (this.heap[leftChild] > element) {
-          swap = leftChild;
+      if (left < length) {
+        if (this.heap[left] > this.heap[index]) {
+          swap = left;
         }
       }
-
-      if (rightChild < length) {
+      if (right < length) {
         if (
-          (swap !== null && this.heap[rightChild] > this.heap[leftChild]) ||
-          (swap === null && this.heap[rightChild] > element)
+          (swap !== null && this.heap[right] > this.heap[left]) ||
+          (swap === null && this.heap[right] > this.heap[index])
         ) {
-          swap = rightChild;
+          swap = right;
         }
       }
-
       if (swap === null) break;
-      this.heap[index] = this.heap[swap];
-      this.heap[swap] = element;
+      this.swap(index, swap);
       index = swap;
     }
   }
 
-  // Method to insert a new node
-  insert(node) {
-    this.heap.push(node);
+  insert(value) {
+    this.heap.push(value);
     this.heapifyUp();
   }
 
-  // Method to remove the root
-  remove() {
-    let removed = this.heap.shift();
-    this.heapifyDown();
-    return removed;
+  delete() {
+    const max = this.heap[0],
+      end = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this.heapifyDown();
+    }
+    return max;
+  }
+
+  print() {
+    let result = [];
+    while (this.heap.length) {
+      result.push(this.delete());
+    }
+    console.log(result);
   }
 }
 
-let maxHeap = new MaxHeap();
-maxHeap.insert(3);
-maxHeap.insert(2);
-maxHeap.insert(1);
-console.log(maxHeap.remove()); // 3
+let heap = new MaxHeap();
+heap.insert(27);
+heap.insert(10);
+heap.insert(17);
+heap.insert(32);
+heap.insert(4);
+heap.insert(56);
+heap.insert(60);
+heap.print();
